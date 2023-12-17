aacd597e57c06d3e06481461f6884f56935cc115
nancymoreen <nancymoreen@gmail.com>
2023-12-17 06:21:17 -0800

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,21 +1,14 @@
 #!/usr/bin/env bash
-# script to set up clean server
-sudo apt-get update
-sudo apt-get install -y nginx
-sudo mkdir /data/
-sudo mkdir /data/web_static/
-sudo mkdir /data/web_static/releases/
-sudo mkdir /data/web_static/shared/
-sudo mkdir /data/web_static/releases/test/
-echo "<!DOCTYPE html>
-<html>
-  <head>
-  </head>
-  <body>
-    Best School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+if ! command -v nginx &>/dev/null; then
+    sudo apt-get update
+    sudo apt-get -y install nginx
+fi
+sudo mkdir -p /data/web_static/{releases/test,shared}
+sudo touch /data/web_static/releases/test/index.html
+echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html >/dev/null
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+nginx_config="/etc/nginx/sites-available/default"
+sudo sed -i '/^\s*server\s*{/a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' "$nginx_config"
 sudo service nginx restart
+exit 0
